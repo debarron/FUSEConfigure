79f8b2775dfb557dd5c90108458732bc6a47c7dc
Daniel Lopez <de.lopezbarron@gmail.com>
2020-06-02 17:38:52 -0600

    Updating local lucy repo

--- fuse-configure.sh
+++ fuse-configure.sh
@@ -12,16 +12,21 @@ wget https://github.com/libfuse/libfuse/releases/download/fuse-3.6.2/fuse-3.6.2.
 tar -xvf fuse-3.6.2.tar.xz
 cd fuse-3.6.2
 mkdir build; cd build
-meson ..
+\$HOME/.local/bin/meson ..
 ninja
 sudo ninja install
+for i in '/lib' '/usr/lib' '/usr/local/lib'
+do 
+  sudo ln -s /usr/local/lib/x86_64-linux-gnu/libfuse3.so \$i/libfuse3.so.3
+done
+
 exit 0
 "
 
 echo ">> FUSE CONFIGURE "
 for machine in $(cat $MACHINES)
 do
-  ssh -i $PRIVATE_KEY -o "StrictHostKeyChecking no" $USER_NAME@$machine "$script"
+  ssh -i $PRIVATE_KEY -o "StrictHostKeyChecking no" $USER_NAME@$machine "$script" > /dev/null
   echo -e "\t + $machine DONE :)"
 done
 
