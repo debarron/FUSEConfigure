619d3b220bf42d09194913e2af6a3ac947b1c052
Daniel Lopez <de.lopezbarron@gmail.com>
2020-06-04 15:35:36 -0600

    Make the script less verbose by seting a dump to /dev/null

--- fuse-configure.sh
+++ fuse-configure.sh
@@ -30,41 +30,9 @@ echo ">> FUSE CONFIGURE "
 bp_list=""
 for machine in $(cat $MACHINES)
 do
-  #ssh -i $PRIVATE_KEY -o "StrictHostKeyChecking no" $USER_NAME@$machine "$script" > /dev/null &
-  ssh -i $PRIVATE_KEY -o "StrictHostKeyChecking no" $USER_NAME@$machine "$script"
+  ssh -i $PRIVATE_KEY -o "StrictHostKeyChecking no" $USER_NAME@$machine "$script" > /dev/null
   echo -e "\t + $machine FINISHED :)"
 done
 
-
-#echo -e "\nWAITING FOR SETUP TO FINISH..\n"
-#TOTAL=$(cat $MACHINES | wc -l | sed 's/ //g')
-#DATE=$(date| tr '[:lower:]' '[:upper:]')
-#echo $DATE
-#echo -e "CHECKING PIDS STATUS.."
-#FINISHED=1
-#while [[ $FINISHED -gt 0 ]]; do
-#	FINISHED=$TOTAL
-#	
-#  states=""
-#	for pid in $bp_list; do 
-#		state=$(ps -o state $pid  |tail -n +2)
-#		states="$states $state"
-#		if [[ ${#state} -eq 0 ]]; then
-#			FINISHED=$((FINISHED-1))
-#		fi;
-#	done;
-#	
-#  #echo $states
-#  echo "REMAINING: "$FINISHED"/"$TOTAL
-#	states=${states// /}
-#	if [[ ${#states} -gt 0 ]]; then
-#		sleep 30
-#	fi
-#done;
-#
-#DATE=$(date| tr '[:lower:]' '[:upper:]')
-#echo $DATE
-#wait
-#
-#echo ">> WORK IS DONE 🥃"
-#exit 0
+echo ">> WORK IS DONE 🥃"
+exit 0
